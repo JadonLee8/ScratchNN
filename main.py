fixed index off by one error

main.py
@@ -3,5 +3,5 @@ import numpy as np
 from nnetwork import network
 
 if __name__ == "__main__":
-    net = network([2, 2, 1], 2)
-    net.feed_forward([1, 1])
+    net = network([16, 16, 4], 748)
+    net.feed_forward(np.random.random(size=748))